b27a036920278170ce29fd08521e4971c3c3db46
CaioCDJ <caiodjesus1@hotmail.com>
2022-06-21 22:01:49 -0300

    Layout Defined

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./index.html"],
+    theme: {
+        colors:{
+            background: '#0C0E14',
+            card:"#181b28",
+            cardHover: "#13151f",
+            subTitles:"#c50ed2",
+            purple: "#781eff" 
+        }, 
+        extend: {},
+    },
+    plugins: [],
 }
